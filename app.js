b549ebf75515af7d3ac49423b90d1be6443d2a98
DESKTOP-LK4POK4\Judit <toth.jdt@gmail.com>
2018-04-14 12:44:34 +0200

    adfasd

---
+++ app.js
@@ -0,0 +1,81 @@
+var http = require('http');
+var mysql = require('mysql');
+var nodemailer = require('nodemailer');
+var hostname = '127.0.0.1';
+var port = 3000;
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : '',
+    password : '',
+    database : ''
+});
+
+var email_user = {
+    user     : '',
+    password : '',
+    host: ''
+};
+
+var transporter = nodemailer.createTransport({
+    host: email_user.host,
+    port: '587',
+    secure: false,
+    auth: {
+        user: email_user.user,
+        pass: email_user.password
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+function dbconnect(callback) {
+    connection.query('SELECT notifications.date_time, notifications.host FROM shadow_trap_id, notifications WHERE shadow_trap_id.trap_id = notifications.trap_id', function(err,row){
+        if (err) {
+            throw err;
+        }
+        return callback(err, row);
+    });
+}
+
+function dbdelete(callback) {
+    connection.query('DELETE FROM shadow_trap_id', function(err,row){
+        if (err) {
+            throw err;
+        }
+        return callback(err, row);
+    });
+}
+
+var server = http.createServer(function(req, res) {
+    res.statusCode = 200;
+});
+
+dbconnect(function(err, result){
+    if (err) throw err;
+    else {
+        if(result.length > 0){
+            result.forEach(function(item){
+                var mailOptions = {
+                    from: email_user.user,
+                    to: 'toth.jdt@gmail.com',
+                    subject: 'Net snmp értesítés.',
+                    text: 'Új bejegyzés történet: ' + item.date_time + ' időpontban, ' + item.host + ' hoston.'
+                };
+            });
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+                return next();
+            });
+        }
+    }
+    return;
+});
+
+server.listen(function(port, hostname){
+});
