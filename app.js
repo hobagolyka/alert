a9552c28a45ce86a4c212d86d1bd45d827063d91
DESKTOP-LK4POK4\Judit <toth.jdt@gmail.com>
2018-04-14 12:58:19 +0200

    asdfasd

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// INIT
 var http = require('http');
 var mysql = require('mysql');
 var nodemailer = require('nodemailer');
 var hostname = '127.0.0.1';
 var port = 3000;
 
+// CONFIG
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : '',
@@ -30,6 +32,9 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+var timer = 60 * 1000;
+
+// FUNCTIONS - DB
 function dbconnect(callback) {
     connection.query('SELECT notifications.date_time, notifications.host FROM shadow_trap_id, notifications WHERE shadow_trap_id.trap_id = notifications.trap_id', function(err,row){
         if (err) {
@@ -48,34 +53,42 @@ function dbdelete(callback) {
     });
 }
 
+// SERVER
 var server = http.createServer(function(req, res) {
     res.statusCode = 200;
 });
 
-dbconnect(function(err, result){
-    if (err) throw err;
-    else {
-        if(result.length > 0){
-            result.forEach(function(item){
+setInterval(function() {
+    dbconnect(function(err, result){
+        if (err) throw err;
+        else {
+            if(result.length > 0){
+                var message = '';
+
+                result.forEach(function(item){
+                    message = message + 'Új bejegyzés történet: ' + item.date_time + ' időpontban, ' + item.host + ' hoston.\n';
+                });
+
                 var mailOptions = {
                     from: email_user.user,
                     to: 'toth.jdt@gmail.com',
                     subject: 'Net snmp értesítés.',
-                    text: 'Új bejegyzés történet: ' + item.date_time + ' időpontban, ' + item.host + ' hoston.'
+                    text: message
                 };
-            });
-            transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-                return next();
-            });
+
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                    return;
+                });
+            }
         }
-    }
-    return;
-});
+        return;
+    });
+}, timer);
 
 server.listen(function(port, hostname){
 });
